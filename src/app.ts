972127a40387080f17398821c1243e2da11f305d
mainalijageswor <159867211+mainalijageswor@users.noreply.github.com>
2024-02-15 21:49:59 +0545

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 require('dotenv').config();
-import IndexRouter from "./Routes/index";
+const IndexRouter = require("./Routes/index");
 const express = require('express');
 const app = express();
 const cors = require("cors");
