ec66bb9fde3cba105cc10c9d86af0b04d84696ce
mainalijageswor <159867211+mainalijageswor@users.noreply.github.com>
2024-02-15 22:10:02 +0545

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const IndexRouter = require("./Routes/index");
+import "dotenv/config";
+import IndexRouter from "./Routes/index";
 const express = require('express');
 const app = express();
 const cors = require("cors");
 
+const PORT: any = process.env.PORT ?? 8085;
 
-app.listen(8085, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`);
 });
 app.use(cors());
 app.use(express.json());
 // app.use(urlencoded({ extended: true }));
 app.use("/api/v1", IndexRouter);
-app.use((req: any, res: any, next: any) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   const error: Error & { status?: number } & { message: String } = new Error();
   error.status = 404;
   error.message = "Page Not Found";
@@ -20,7 +21,7 @@ app.use((req: any, res: any, next: any) => {
 });
 
 //Error handler middleware
-app.use((error: any, req: any, res: any, next: any) => {
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   res.status(error.status || 500).json({
     status: false,
     message: error.message,
